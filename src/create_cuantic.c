66ed9c48ad128647a79e82b91a363217f2477b79
Luis-Bellot <43735795+Luis-Bellot@users.noreply.github.com>
2021-01-05 20:17:47 +0100

    Update create_cuantic.c

--- src/create_cuantic.c
+++ src/create_cuantic.c
@@ -2,7 +2,7 @@
 #include "defines.h"
 
 /**
- * @param Cuantic * cuantic --> structure that stores cuantic numbers
+ * @param Cuantic * cuantic --> structure that stores quantum numbers
  * @param double sl 
  * @param double ll
  * @param double jl
@@ -234,7 +234,7 @@ int Cuanten(Cuantic *cuantic, PRECISION sl, PRECISION ll, PRECISION jl, PRECISIO
 	cuantic->FO = fos;
 
 	if(log){
-		printf("\n\n------------    QUANTUM NUMBERS   --------------");
+		printf("\n\n--------------    QUANTUM NUMBERS  -- --------------");
 		printf("\nNumber of Pi components:\t\t %lf",cuantic->N_PI);
 		printf("\nNumber of Sigma components:\t\t %lf",cuantic->N_SIG);
 		printf("\nLower level lande factor:\t\t %lf",cuantic->GL);
@@ -283,7 +283,7 @@ int Cuanten(Cuantic *cuantic, PRECISION sl, PRECISION ll, PRECISION jl, PRECISIO
 				printf(" %lf ",cuantic->WER[i]);
 		}
 		printf("\nRelative line strength:\t\t\t %lf",cuantic->FO);
-		printf("\n------------------------------------------------\n");	
+		printf("\n----------------------------------------------------\n");	
 	}
 
 	free(m1);
